a7a12479d3ccf54000b4ac722cec796b5c295c95
Lewis <lewisbirch2@googlemail.com>
2024-11-04 12:00:47 +0000

    Completed T5 Single article endpoint

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./Header";
 import Articles from "./Articles";
+import { Routes, Route } from "react-router-dom";
+import SingleArticle from "./SingleArticle";
 
 function App() {
   return (
     <>
       <Header />
-      <Articles/>
+      <Routes>
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
+      </Routes>
     </>
   );
 }
