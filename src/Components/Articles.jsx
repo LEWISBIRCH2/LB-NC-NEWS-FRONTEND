7e4a65517b8db9015dddcb313963bb37b9d687e2
Lewis <lewisbirch2@googlemail.com>
2024-11-06 15:34:23 +0000

    Ticket 11 - Sorting

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -7,8 +7,8 @@ import { useLocation } from "react-router-dom";
 export default function Articles() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const URL = useLocation().search.slice(7);
+  const [filterTarget, setFilterTarget] = useState("title");
+  const [isAscending, setIsAscending] = useState("asc");
 
   useEffect(() => {
     setLoading(true);
@@ -23,20 +23,56 @@ export default function Articles() {
       });
   }, []);
 
+  function handleSort(event) {
+    if (event.target.value === "Title") {
+      setFilterTarget("title");
+    }
+    if (event.target.value === "Date") {
+      setFilterTarget("created_at");
+    }
+    if (event.target.value === "Votes") {
+      setFilterTarget("votes");
+    }
+  }
+
+  function handleOrder(event) {
+    if (event.target.value === "Ascending") {
+      setIsAscending("asc");
+    }
+    if (event.target.value === "Descending") {
+      setIsAscending("desc");
+    }
+  }
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://lb-nc-news.onrender.com/api/articles?sort_by=${filterTarget}&order=${isAscending}`
+      )
+      .then((response) => {
+        setArticles(response.data.articles);
+      })
+      .catch((error) => {
+        console.log(error, "UNSUCCESSFUL");
+      });
+  }, [filterTarget, isAscending]);
+
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  if (URL !== "") {
+  let TopicURL = useLocation().search.slice(7);
+
+  if (TopicURL !== "") {
     return (
       <>
-        <h2> Here's all the articles based on {URL} </h2>
+        <h2> Here's all the articles based on {TopicURL} </h2>
         <Link to={`/articles`}>
           <button>Back to all articles</button>
         </Link>
         <ul>
           {articles.map((article) => {
-            if (article.topic === URL)
+            if (article.topic === TopicURL)
               return (
                 <Link
                   to={`/articles/${article.article_id}`}
@@ -57,15 +93,33 @@ export default function Articles() {
         <h2>Browse by Topic:</h2>
         {<FilterTopics />}
       </section>
+      <section>
+        <h2> Sort by... </h2>
+        <select onChange={handleSort}>
+          <option>Title </option>
+          <option>Date </option>
+          <option>Votes </option>
+        </select>
+        <select onChange={handleOrder}>
+          <option>Ascending</option>
+          <option>Descending</option>
+        </select>
+      </section>
 
       <h2> Here's a list of all available articles... </h2>
       {articles.map((article) => {
         return (
           <Link to={`/articles/${article.article_id}`} key={article.article_id}>
-            <li>
+            <ol>
               {" "}
-              <ul>{article.title}</ul>{" "}
-            </li>
+              <ul>
+                {article.title} <br></br>
+                Date: {article.created_at}
+                <br></br>
+                Votes: {article.votes}
+                <br></br>
+              </ul>{" "}
+            </ol>
           </Link>
         );
       })}
